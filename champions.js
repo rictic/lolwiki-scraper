b48ad4e58595deac50d56e50a3f56db9e51d919a
Peter Burns <rictic@gmail.com>
2012-01-23 22:12:38 -0800

    tabs to spaces

--- champions.js
+++ champions.js
@@ -37,63 +37,63 @@ champion_url_list.forEach(function(champion_url) {
       champion[stat.replace("\\", "")] = info[1];
     });
 
-	var abilitiesMap = {}
-	var abilityTable = body.match(/class="abilities_table"[^]*?\<\/table\>/)[0];
-	
-	var passive = abilityTable.match(/\<tr class="ability innate_ability"\>[^]*?\<\/tr\>/g)[0];
-	var passiveName = passive.match(/\<td id="(.*?)"/)[0];
-	var passiveInfo = passive.match(/\<td class="abilityinfo"[^]*?\<\/tr\>/)[0];
-	champion["passive"] = passiveName + " -- " + passiveInfo;
-	
-	var abilities = abilityTable.match(/\<tr class="ability (basic|ultimate)_ability"\>[^]*?\<\/tr\>/g);
-	var abilityNames = abilityTable.match(/\<td id="(.*?)"/g);
-	var abilityInfos = abilityTable.match(/\<td class="abilityinfo"[^]*?\<\/td\>/g);
-	
-	for (var i=0; i<abilities.length; i++) {
-		var ability = abilities[i];
-		console.log("Ability: " + ability);
-		
-		var name = ability.match(/\<td id="(.*?)"/)[1];
-		console.log("Ability name: " + name);
-		
-		var abilityInfo = ability.match(/\<td class="abilityinfo"[^]*?\<\/td\>/)[0];
-		console.log("Ability Info: " + abilityInfo);
-		var cost1 = abilityInfo.match(/\<b\>Cost:\<\/b\>[^]*?\<\/li\>/);
-		console.log("Cost 1: " + cost1);
-		var cooldown1 = abilityInfo.match(/\<b\>Cooldown:\<\/b\>[^]*?\<\/li\>/);
-		console.log("Cooldown 1: " + cooldown1);
-		var range1 = abilityInfo.match(/\<b\>Range:\<\/b\>[^]*?\<\/li\>/);
-		console.log("Range 1: " + range1);
-		
-		var abilityLevels = ability.match(/\<td class="abilitylevel"[^]*?\<\/td\>/)[0];
-		console.log("Ability levels: " + abilityLevels);
-		var cost2 = abilityLevels.match(/\<b\>Cost:\<\/b\>[^]*?\<\/p\>/);
-		console.log("Ability cost: " + cost2);
-		var cooldown2 = abilityLevels.match(/\<b\>Cooldown:\<\/b\>[^]*?\<\/p\>/);
-		console.log("Ability cooldown: " + cooldown2);
-		var range2 = abilityLevels.match(/\<b\>Range:\<\/b\>[^]*?\<\/p\>/);
-		console.log("Ability range: " + range2);
-		
-		champion["ability " + i] = name;
-		
-		if (cost1 != null) {
-			champion["ability " + i + " cost"] = cost1;
-		} else {
-			champion["ability " + i + " cost"] = cost2;
-		}
-		
-		if (cooldown1 != null) {
-			champion["ability " + i + " cooldown"] = cooldown1;
-		} else {
-			champion["ability " + i + " cooldown"] = cooldown2;
-		}
-		
-		if (range1 != null) {
-			champion["ability " + i + " range"] = range1;
-		} else {
-			champion["ability " + i + " range"] = range2;
-		}
-	}
+  var abilitiesMap = {}
+  var abilityTable = body.match(/class="abilities_table"[^]*?\<\/table\>/)[0];
+  
+  var passive = abilityTable.match(/\<tr class="ability innate_ability"\>[^]*?\<\/tr\>/g)[0];
+  var passiveName = passive.match(/\<td id="(.*?)"/)[0];
+  var passiveInfo = passive.match(/\<td class="abilityinfo"[^]*?\<\/tr\>/)[0];
+  champion["passive"] = passiveName + " -- " + passiveInfo;
+  
+  var abilities = abilityTable.match(/\<tr class="ability (basic|ultimate)_ability"\>[^]*?\<\/tr\>/g);
+  var abilityNames = abilityTable.match(/\<td id="(.*?)"/g);
+  var abilityInfos = abilityTable.match(/\<td class="abilityinfo"[^]*?\<\/td\>/g);
+  
+  for (var i=0; i<abilities.length; i++) {
+    var ability = abilities[i];
+    console.log("Ability: " + ability);
+    
+    var name = ability.match(/\<td id="(.*?)"/)[1];
+    console.log("Ability name: " + name);
+    
+    var abilityInfo = ability.match(/\<td class="abilityinfo"[^]*?\<\/td\>/)[0];
+    console.log("Ability Info: " + abilityInfo);
+    var cost1 = abilityInfo.match(/\<b\>Cost:\<\/b\>[^]*?\<\/li\>/);
+    console.log("Cost 1: " + cost1);
+    var cooldown1 = abilityInfo.match(/\<b\>Cooldown:\<\/b\>[^]*?\<\/li\>/);
+    console.log("Cooldown 1: " + cooldown1);
+    var range1 = abilityInfo.match(/\<b\>Range:\<\/b\>[^]*?\<\/li\>/);
+    console.log("Range 1: " + range1);
+    
+    var abilityLevels = ability.match(/\<td class="abilitylevel"[^]*?\<\/td\>/)[0];
+    console.log("Ability levels: " + abilityLevels);
+    var cost2 = abilityLevels.match(/\<b\>Cost:\<\/b\>[^]*?\<\/p\>/);
+    console.log("Ability cost: " + cost2);
+    var cooldown2 = abilityLevels.match(/\<b\>Cooldown:\<\/b\>[^]*?\<\/p\>/);
+    console.log("Ability cooldown: " + cooldown2);
+    var range2 = abilityLevels.match(/\<b\>Range:\<\/b\>[^]*?\<\/p\>/);
+    console.log("Ability range: " + range2);
+    
+    champion["ability " + i] = name;
+    
+    if (cost1 != null) {
+      champion["ability " + i + " cost"] = cost1;
+    } else {
+      champion["ability " + i + " cost"] = cost2;
+    }
+    
+    if (cooldown1 != null) {
+      champion["ability " + i + " cooldown"] = cooldown1;
+    } else {
+      champion["ability " + i + " cooldown"] = cooldown2;
+    }
+    
+    if (range1 != null) {
+      champion["ability " + i + " range"] = range1;
+    } else {
+      champion["ability " + i + " range"] = range2;
+    }
+  }
 
     champions.push(champion);
     console.log(champion);
